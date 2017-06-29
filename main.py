a2aa9985bb5c3b90acc69c2fca314c718b203e0a
Igor Barsi <ibarsi@unioncreative.com>
2017-06-29 16:11:31 -0400

    - Added some defensive code in case no images are found.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from scrape import get_random_image
 
 image_file_path = os.path.join('/Library/Desktop Pictures/', IMAGE_FILENAME)
 
+image = get_random_image()
+
 # Update desktop image & restart Dock
-urllib.urlretrieve(get_random_image(), image_file_path)
-call('killall Dock', shell=True)
+if image is not None:
+    urllib.urlretrieve(image, image_file_path)
+    call('killall Dock', shell=True)
