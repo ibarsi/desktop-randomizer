ac7dd12f2d7665b1fdad0bedc1c1fca5faa8096e
Igor Barsi <ibarsi92@gmail.com>
2018-01-14 09:57:45 -0500

    Added logging and main function to entry file. Enabled VS Code debugging.

--- main.py
+++ main.py
@@ -2,18 +2,29 @@
 
 import os
 import urllib
+import sys
 from subprocess import call
 
 from settings import IMAGE_FILENAME
 from scrape import get_random_image
 
-image_file_path = os.path.join('/Library/Desktop Pictures/', IMAGE_FILENAME)
 
-image = get_random_image()
+def main():
+    print "=== START ==="
 
-# Update desktop image & restart Dock
-if image is not None:
-    urllib.urlretrieve(image, image_file_path)
-    call('killall Dock', shell=True)
-else:
-    print "WARNING: No image was returned from scraper."
+    image_file_path = os.path.join('/Library/Desktop Pictures/', IMAGE_FILENAME)
+    image = get_random_image()
+
+    # Update desktop image & restart Dock
+    if image is not None:
+        print "Found image " + image
+
+        urllib.urlretrieve(image, image_file_path)
+        call('killall Dock', shell=True)
+
+        sys.exit(0)
+    else:
+        print "WARNING: No image was returned from scraper."
+
+if __name__ == "__main__":
+    main()
