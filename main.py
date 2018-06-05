74adf2c0e62441947ae2679708b9002fe1789c6d
Igor Barsi <ibarsi92@gmail.com>
2018-06-04 22:17:45 -0400

    ♻️ Added additional logs and updated to latest urllib.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-import urllib
+import urllib2
 import sys
 from subprocess import call
 
@@ -13,18 +13,27 @@ def main():
     print "=== START ==="
 
     image_file_path = os.path.join('/Library/Desktop Pictures/', IMAGE_FILENAME)
-    image = get_random_image()
+    image_url = get_random_image()
 
     # Update desktop image & restart Dock
-    if image is not None:
-        print "Found image " + image
+    if image_url is not None:
+        print "Found image " + image_url
+        print "Saving to " + image_file_path
 
-        urllib.urlretrieve(image, image_file_path)
+        image_response = urllib2.urlopen(image_url)
+        image_buffer = image_response.read()
+
+        with open(image_file_path, 'wb') as image_file:
+            image_file.write(image_buffer)
+
+        image_response.close()
+
         call('killall Dock', shell=True)
 
         sys.exit(0)
     else:
         print "WARNING: No image was returned from scraper."
+
 
 if __name__ == "__main__":
     main()
